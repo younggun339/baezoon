a4fd3c715bc450a8cacf39d4192602b1a5066c5b
younggun339 <152257708+younggun339@users.noreply.github.com>
2023-12-26 15:26:57 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a = input();
+if int(a)>=90 : 
+    print("A");
+elif 90>int(a)>=80 :
+    print("B");
+elif 80>int(a)>=70 :
+    print("C");
+elif 70>int(a)>=60 :
+    print("D");
+else :
+    print("F");
