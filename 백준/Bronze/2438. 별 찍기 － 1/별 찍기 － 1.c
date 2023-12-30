29d5f2419433a110da67ff0a9df4702a50054fde
younggun339 <152257708+younggun339@users.noreply.github.com>
2023-12-30 22:05:05 +0900

    Update 별 찍기 － 1.c
    시도했다가 실패한 코드 첨부

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -13,3 +13,18 @@ int main() {
     return 0;
     
 }
+
+// int main() {
+//     int number;
+//     char star[200];
+//     char plusStar = "*";
+//     scanf("%d", &number);
+//     for(int i = 0; i < number; i++){
+//         strcat(star,plusStar);
+//         printf("%s\n",star);
+//     }
+//     return 0;
+    
+// }
+// 하면 star의 타입문제 때문에 프린트에서 자료형을 찾지 못하는 경우.
+// 그렇다면 정적공간을 할당한 문자열은 프린트할 수 없단말인가? 
