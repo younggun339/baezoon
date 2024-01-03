73da7f8e442f2f6ec4f1996500652c3345d68977
younggun339 <152257708+younggun339@users.noreply.github.com>
2024-01-03 15:44:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char sequence[101];
+    // int count = 0;
+    scanf("%s", sequence);
+    // int i = 0;
+    printf("%ld", strlen(sequence));
+
+    // printf("%s", sequence);
+    // printf("%s %s", sequence[0], sequence[1]);
+
+    // for(int i = 0; i<101; i++);{
+    //     printf("%d\n",sequence[i]);
+    //     // if(sequence[i] != 0){
+    //     //  count = count +1;   
+    //     //  printf("%d\n",sequence[i]);
+    //     // }
+    // }
+    // printf("%d", count);
+    // printf("%lu", sizeof(sequence));
+}
